1a17fdfa1fc0a2f4437584d407d3db3c8b7a6cd1
Alexey Gedz <alexeygedz@gmail.com>
2023-12-22 14:16:32 +0500

    Базовый роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
