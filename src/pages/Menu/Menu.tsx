c2da6e43ec64a5f9a9089df7697b73515c1bb728
Alexey Gedz <alexeygedz@gmail.com>
2023-12-29 15:32:54 +0500

    Вложенная страница продукта

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from '../../components/Headling/Headling';
+import ProductCart from '../../components/ProductCart/ProductCart';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 
@@ -7,6 +8,16 @@ export function Menu() {
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>
 			<Search placeholder='Введите блюдо или состав'/>
+		</div>
+		<div>
+			<ProductCart
+				id={1}
+				title='Наслаждение'
+				description='Салями, руккола, помидоры, оливки'
+				rating={4.5}
+				price={300}
+				image='product-demo.png'
+			/>
 		</div>
 	</>;
 }
